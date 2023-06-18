150645a6cde893b48fc6895f3abfda4160c1cbf4
fanning123456 <2408727768@qq.com>
2023-06-18 23:40:28 +0800

    ni'haonihao
    1

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -37,7 +37,7 @@ export default {
           { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur' }
         ],
         confirmPassword: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
+          { required: true, message: '请输入密码 ', trigger: 'blur' },
           { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur' }
         ],
       }
