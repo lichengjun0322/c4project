05dd513fa8bf61fef8be244beca61b80b7a4261a
fanning123456 <2408727768@qq.com>
2023-06-21 01:39:51 +0800

    1223
    12121

--- vue/src/views/analysic.vue
+++ vue/src/views/analysic.vue
@@ -6,7 +6,7 @@
                     <el-input v-model="all_name.name" autocomplete="off" width="200"></el-input>
                 </el-form-item>
                 <el-form-item label="视频文件" :label-width="formLabelWidth">
-                    <!-- 这里的action为服务器地址 -->
+                    
                     <el-upload class="upload-demo" :data="all_name" ref="upload" :auto-upload="false" drag
                         action="https://jsonplaceholder.typicode.com/posts/" multiple>
                         <i class="el-icon-upload"></i>
@@ -22,8 +22,6 @@
             </div>
         </el-dialog>
 
-
-
         <div class="header_01">
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item :to="{ path: '/shouye' }">首页</el-breadcrumb-item>
@@ -32,24 +30,29 @@
         </div>
 
 
-
         <div class="all_button">
-            <el-button @click="this.dialogFormVisible=true">上传视频</el-button>
+            <el-button @click="dialogFormVisible=true">上传视频</el-button>
             <el-button @click="openFullScreen1" v-loading.fullscreen.lock="fullscreenLoading">开始转换</el-button>
         </div>
-        <div class="bodyer">
-            <vue-core-video-player :src="1" ref="video_video" class="vid"></vue-core-video-player>
-            <vue-core-video-player :src="1" ref="video_video" class="vid"></vue-core-video-player>
+
+        
+        <div class="bodyer_01">
+            <vue-core-video-player :src="require('../../public/video/2.mp4')" ref="video_video_01" class="vid_01"></vue-core-video-player>
+            
+            <vue-core-video-player :src="require('../../public/video/2.mp4')" ref="video_video_02" class="vid_02"></vue-core-video-player>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    name: 'analysic',
     data() {
         return {
             fullscreenLoading: false,
             dialogFormVisible: false,
+            formLabelWidth:200,
+            all_name:[]
 
 
         };
@@ -57,11 +60,13 @@ export default {
     methods: {
         queding(){
             this.dialogFormVisible = false;
+            this.$refs.video_video_01.src=require("../../public/video/origin.mp4");
         },
         openFullScreen1() {
             this.fullscreenLoading = true;
             setTimeout(() => {
                 this.fullscreenLoading = false;
+                this.$refs.video_video_02.src=require("../../public/video/2.mp4");
                 this.$message({
                     message: '转换成功',
                     type: 'success'
@@ -74,6 +79,8 @@ export default {
 
 <style>
 .all {
+    height:100%;
+    width:100%;
     display:flex;
     flex-direction: column;
     justify-content: center;
@@ -81,27 +88,36 @@ export default {
 }
 .header_01 {
     width:100%;
-    height:10%;
+    height:5%;
     background-color: rgb(240,240,240);
+    margin-bottom: 10px;
 }
 .all_button {
     width:100%;
-    height:10%;
+    height:7%;
+    
 
 }
 .all_button>.el-button {
     width:10%;
     height:100%;
+
 }
 .bodyer_01 {
     width:100%;
-    height:80%;
+    height:87%;
     display:flex;
     flex-direction: row;
-    justify-content: space-around;
+    justify-content:flex-start;
     align-items: center;
 }
-.bodyer_01>.vid {
+.bodyer_01>.vid_01 {
+    display:block;
+    width:40%;
+    height:80%;
+}
+
+.bodyer_01>.vid_02 {
     display:block;
     width:40%;
     height:80%;
